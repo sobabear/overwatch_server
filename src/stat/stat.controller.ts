ee0ba5a5942bee75e552f78fa248a64628eef346
Yongjun <stareta1202@gmail.com>
2023-07-28 14:52:22 +0900

    WIP

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { StatService } from './stat.service';
 
 @Controller('stat')
-export class StatController {}
+export class StatController {
+    constructor(private readonly overwatchService: StatService) {}
+
+    @Get(':platform/:region/:tag')
+    async getOverwatchStats(
+      @Param('platform') platform: string,
+      @Param('region') region: string,
+      @Param('tag') tag: string,
+    ): Promise<any> {
+      const html = await this.overwatchService.getHTML(platform, region, tag);
+      const parsedData = this.overwatchService.parseHTML(html);
+      return parsedData;
+    }
+}
--- src/stat/stat.service.ts
+++ src/stat/stat.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import cheerio from 'cheerio';
+import { retryRequest } from 'src/utils/utils';
+
+const MAX_RETRIES = 3;
 
 @Injectable()
-export class StatService {}
+export class StatService {
+  async getHTML(platform: string, region: string, tag: string): Promise<string> {
+    const url = `https://overwatch.blizzard.com/en-us/career/${tag}/`;
+    const ua =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0';
+
+    const options = {
+      uri: encodeURI(url),
+      headers: {
+        'User-Agent': ua,
+      },
+      encoding: 'utf8',
+    };
+
+    return retryRequest(options, MAX_RETRIES);
+  }
+
+  parseHTML(html: string): any {
+    const $ = cheerio.load(html);
+
+    const isFound = $('.heading').text() !== 'Page Not Found';
+    if (!isFound) {
+      throw new Error('Profile not found');
+    }
+
+    // Your parsing logic goes here...
+
+    return { stats, parsed };
+  }
+}
